733a118ef305b354f9dbd69844300c54144fbffc
Jarek Sedlacek <jareksedlacek@gmail.com>
2017-12-02 17:03:52 -0600

    Add Links method, using external library (https://github.com/mvdan/xurls) for url parsing. Also added tests. The tests are not hermetic (they actually reach out and read the URLs, I need to go back and move out the http requests)

--- parser/parser.go
+++ parser/parser.go
@@ -1,16 +1,72 @@
 // Package parser parses text to identify special elements
 package parser
 
-import "regexp"
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"mvdan.cc/xurls"
+	"net/http"
+	"regexp"
+)
 
 var mentionsRegex = regexp.MustCompile(`@(\w+)`)
 
-func Mentions(message string) []string {
-  found := mentionsRegex.FindAllStringSubmatch(message, -1)
-  mentions := []string{}
-  for _, item := range found {
-    mentions = append(mentions, item[1])
-  }
-  return mentions
+type Link struct {
+	URL   string
+	Title string
 }
 
+func Mentions(message string) []string {
+	found := mentionsRegex.FindAllStringSubmatch(message, -1)
+	mentions := []string{}
+	for _, item := range found {
+		mentions = append(mentions, item[1])
+	}
+	return mentions
+}
+
+func Links(message string) ([]Link, error) {
+	urls := xurls.Strict().FindAllString(message, -1)
+	links := []Link{}
+	for _, url := range urls {
+		title, err := getTitle(url)
+		if err != nil {
+			return nil, err
+		}
+		link := Link{URL: url, Title: title}
+		links = append(links, link)
+	}
+	return links, nil
+}
+
+func isTitleElement(n *html.Node) bool {
+	return n.Type == html.ElementNode && n.Data == "title"
+}
+
+func traverse(n *html.Node) (string, error) {
+	if isTitleElement(n) {
+		return n.FirstChild.Data, nil
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		result, err := traverse(c)
+		if err == nil {
+			return result, nil
+		}
+	}
+
+	return "", fmt.Errorf("never found <title> element")
+}
+
+func getTitle(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return traverse(doc)
+}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -2,63 +2,117 @@ package parser
 
 import "testing"
 
+func linksEqualIgnoringOrder(first []Link, second []Link) bool {
+	firstinterface := make([]interface{}, len(first))
+	for i, v := range first {
+		firstinterface[i] = v
+	}
+	secondinterface := make([]interface{}, len(second))
+	for i, v := range second {
+		secondinterface[i] = v
+	}
+	return itemsEqualIgnoringOrder(firstinterface, secondinterface)
+}
+
+func stringsEqualIgnoringOrder(first []string, second []string) bool {
+	firstinterface := make([]interface{}, len(first))
+	for i, v := range first {
+		firstinterface[i] = v
+	}
+	secondinterface := make([]interface{}, len(second))
+	for i, v := range second {
+		secondinterface[i] = v
+	}
+	return itemsEqualIgnoringOrder(firstinterface, secondinterface)
+}
+
 // itemsEqualIgnoringOrder return true if the two slices contain identical elements,
 // even if they may be in different orders. Duplicates matter, ["a", "a"] != ["a"]
-func itemsEqualIgnoringOrder(first []string, second []string) bool {
-  // if lengths are equal, one slice must have elements not in the other
-  if len(first) != len(second) {
-    return false
-  }
+func itemsEqualIgnoringOrder(first []interface{}, second []interface{}) bool {
+	// if lengths are equal, one slice must have elements not in the other
+	if len(first) != len(second) {
+		return false
+	}
 
-  items := map[string]int{}
-  // Count occurences in first
-  for _, item := range first {
-    items[item]++
-  }
-  // Check everything in second
-  for _, item := range second {
-    // if this wasn't in first (or we've already found a value in second for every value in first),
-    // the slices aren't equal
-    if items[item] == 0 {
-      return false
-    }
-    items[item]--
-  }
-  // If the count isn't zero, item was incremented more than decremented (found more in first)
-  // or decremented more than incremented (found more in second)
-  for _, count := range items {
-    if count != 0 {
-      return false
-    }
-  }
-
-  return true
+	items := map[interface{}]int{}
+	// Count occurences in first
+	for _, item := range first {
+		items[item]++
+	}
+	// Check everything in second
+	for _, item := range second {
+		// if this wasn't in first (or we've already found a value in second for every value in first),
+		// the slices aren't equal
+		if items[item] == 0 {
+			return false
+		}
+		items[item]--
+	}
+	// If the count isn't zero, item was incremented more than decremented (found more in first)
+	// or decremented more than incremented (found more in second)
+	for _, count := range items {
+		if count != 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func TestMentions(t *testing.T) {
-  input := "@chris are you around"
-  expected := []string{"chris"}
-  found := Mentions(input)
-  if !itemsEqualIgnoringOrder(found, expected) {
-    t.Errorf("Mentions(%q): expected %q got %q", input, expected, found)
-  }
+	input := "@chris are you around"
+	expected := []string{"chris"}
+	found := Mentions(input)
+	if !stringsEqualIgnoringOrder(found, expected) {
+		t.Errorf("Mentions(%q): expected %q got %q", input, expected, found)
+	}
 }
 
 func TestMentionsNoMatches(t *testing.T) {
-  input := "Yo, are you around"
-  expected := []string{}
-  found := Mentions(input)
-  if !itemsEqualIgnoringOrder(found, expected) {
-    t.Errorf("Mentions(%q): expected %q got %q", input, expected, found)
-  }
+	input := "Yo, are you around"
+	expected := []string{}
+	found := Mentions(input)
+	if !stringsEqualIgnoringOrder(found, expected) {
+		t.Errorf("Mentions(%q): expected %q got %q", input, expected, found)
+	}
 }
 
-
 func TestMultipleMatches(t *testing.T) {
-  input := "@bob @john (success) such a cool feature; https://twitter.com/jdorfman/status/430511497475670016"
-  expected := []string{"bob", "john"}
-  found := Mentions(input)
-  if !itemsEqualIgnoringOrder(found, expected) {
-    t.Errorf("Mentions(%q): expected %q got %q", input, expected, found)
-  }
+	input := "@bob @john (success) such a cool feature; https://twitter.com/jdorfman/status/430511497475670016"
+	expected := []string{"bob", "john"}
+	found := Mentions(input)
+	if !stringsEqualIgnoringOrder(found, expected) {
+		t.Errorf("Mentions(%q): expected %q got %q", input, expected, found)
+	}
+}
+
+// TODO(jarek): mock out http calls, so tests are hermetic
+func TestLinks(t *testing.T) {
+	input := "@bob @john (success) such a cool feature; https://twitter.com/jdorfman/status/430511497475670016"
+	expected := []Link{
+		{
+			URL:   "https://twitter.com/jdorfman/status/430511497475670016",
+			Title: "Justin Dorfman; on Twitter: \"nice @littlebigdetail from @HipChat (shows hex colors when pasted in chat). http://t.co/7cI6Gjy5pq\"",
+		},
+	}
+	found, err := Links(input)
+	if err != nil {
+		t.Errorf("Unexpected error from Links(%q): %v", input, err)
+	}
+	if !linksEqualIgnoringOrder(found, expected) {
+		t.Errorf("Links(%q): expected %q got %q", input, expected, found)
+	}
+	input = "Olympics are starting soon; http://www.nbcolympics.com"
+	expected = []Link{
+		{
+			URL:   "http://www.nbcolympics.com",
+			Title: "2018 PyeongChang Olympic Games | NBC Olympics",
+		},
+	}
+	found, err = Links(input)
+	if err != nil {
+		t.Errorf("Unexpected error from Links(%q): %v", input, err)
+	}
+	if !linksEqualIgnoringOrder(found, expected) {
+		t.Errorf("Links(%q): expected %q got %q", input, expected, found)
+	}
 }
